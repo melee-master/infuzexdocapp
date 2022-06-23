750f538ee95048de4178f02483aa14f88e2524c0
Awanish Mishra <55555736+hsinawa@users.noreply.github.com>
2022-06-24 04:59:00 +0530

    Add files via upload

--- client/src/users/updateuserprofile.js
+++ client/src/users/updateuserprofile.js
@@ -7,6 +7,14 @@ import style from './update.css'
 
 const UpdateUserProfile=()=>{
 
+
+    const bengali = localStorage.getItem('bengali')
+    const english = localStorage.getItem('english')
+
+    
+
+
+
     const updatereducer = useSelector(state=>state.UpdateUserReducer)
     const {loading , success , error } = updatereducer
 
@@ -55,8 +63,116 @@ const UpdateUserProfile=()=>{
     return(
         <div>
 
-            <br/><br/>
-           
+{
+        bengali ? ( <p>
+
+<div 
+            style={{width:'80%' , marginLeft:'auto' , marginRight:'auto'  }} 
+            
+            //className="forminput" 
+            
+            >
+
+            <form onSubmit={updateuser} >
+                     
+
+                            <input type="text" value={lname} placeholder='নামের প্রথম অংশ' 
+                            
+                            //style={{ width:'110%' , marginLeft:'auto' , marginRight:'80px' }}
+
+                            //style={{fontSize:'25px'}}
+
+                            id='formtext'
+                            
+                            />
+
+                            <input type="text" value={name} 
+                            placeholder='নতুন নাম লিখুন'
+
+                                onChange={(e) => { setname(e.target.value) }} required
+
+                                id='formtext'
+
+                            />
+
+                      
+
+                        <br />
+
+                        
+
+                            <input type="text" value={newmail} placeholder='ইমেইল আইডি' 
+                             id='formtext'
+                            />
+
+                            <input type="text" value={email} placeholder='নতুন ইমেইল লিখুন'
+
+                                onChange={(e) => { setemail(e.target.value) }}
+
+                                id='formtext'
+
+                                required
+
+                            />
+
+                      
+
+
+                        <br />
+
+                       
+
+                            <input type="password" value={password} placeholder='নতুন পাসওয়ার্ড'
+
+                                onChange={(e) => { setnewpass(e.target.value) }}
+
+                                id='formtext'
+                                required
+
+                            />
+
+                            <input type="password" value={cnewpass} placeholder='পাসওয়ার্ড নিশ্চিত করুন'
+
+                                onChange={(e) => { setcnewpass(e.target.value) }}
+
+                                id='formtext'
+
+                                required
+                            />
+
+                     
+
+
+
+                        <br /> <br />
+
+
+
+                        <button  value='submit' 
+                        
+                        id='formbutton'
+                        //className='boxb'
+                        
+                        style={{color:'white' , backgroundColor:'black' , fontSize:'23px' }}>
+                            {/* <i class="fa fa-refresh" aria-hidden="true"></i> */}
+                            হালনাগাদ</button>
+
+                        <br />
+
+                        <span id="message" style={{ fontSize: "25px", color: '#023047' }} ></span>
+                        { success && ( <span  style={{ fontSize: "25px", color: '#023047' }} >সফলভাবে আপডেট করা হয়েছে</span>  ) }
+                        { error && ( <span  style={{ fontSize: "25px", color: '#023047' }} >কিছু ভুল হয়েছে</span>  ) }
+                    </form>
+
+
+            </div>
+
+
+        </p> ) : ( <p>
+          {
+            english ? (
+              <p>
+
 
             <div 
             style={{width:'80%' , marginLeft:'auto' , marginRight:'auto'  }} 
@@ -157,6 +273,125 @@ const UpdateUserProfile=()=>{
 
 
             </div>
+
+
+              </p>
+            ) : (
+              <p>
+
+
+            <div 
+            style={{width:'80%' , marginLeft:'auto' , marginRight:'auto'  }} 
+            
+            //className="forminput" 
+            
+            >
+
+            <form onSubmit={updateuser} >
+                     
+
+                            <input type="text" value={lname} placeholder='First Name' 
+                            
+                            //style={{ width:'110%' , marginLeft:'auto' , marginRight:'80px' }}
+
+                            //style={{fontSize:'25px'}}
+
+                            id='formtext'
+                            
+                            ></input>
+
+                            <input type="text" value={name} placeholder='Enter New Name'
+
+                                onChange={(e) => { setname(e.target.value) }} required
+
+                                id='formtext'
+
+                            />
+
+                      
+
+                        <br />
+
+                        
+
+                            <input type="text" value={newmail} placeholder='Email Id' 
+                             id='formtext'
+                            />
+
+                            <input type="text" value={email} placeholder='Enter New Email'
+
+                                onChange={(e) => { setemail(e.target.value) }}
+
+                                id='formtext'
+
+                                required
+
+                            />
+
+                      
+
+
+                        <br />
+
+                       
+
+                            <input type="password" value={password} placeholder='New Password'
+
+                                onChange={(e) => { setnewpass(e.target.value) }}
+
+                                id='formtext'
+                                required
+
+                            />
+
+                            <input type="password" value={cnewpass} placeholder='Confirm Password'
+
+                                onChange={(e) => { setcnewpass(e.target.value) }}
+
+                                id='formtext'
+
+                                required
+                            />
+
+                     
+
+
+
+                        <br /> <br />
+
+
+
+                        <button  value='submit' 
+                        
+                        id='formbutton'
+                        //className='boxb'
+                        
+                        style={{color:'white' , backgroundColor:'black' , fontSize:'23px' }}>
+                            {/* <i class="fa fa-refresh" aria-hidden="true"></i> */}
+                            Update</button>
+
+                        <br />
+
+                        <span id="message" style={{ fontSize: "25px", color: '#023047' }} ></span>
+                        { success && ( <span  style={{ fontSize: "25px", color: '#023047' }} >Updated Successfully</span>  ) }
+                        { error && ( <span  style={{ fontSize: "25px", color: '#023047' }} >Something Went Wrong</span>  ) }
+                    </form>
+
+
+            </div>
+
+
+                </p>
+            )
+          }
+        </p> )
+      }
+
+
+
+           
+           
+
 
 
         </div>
--- client/src/users/userprofile.js
+++ client/src/users/userprofile.js
@@ -5,15 +5,93 @@ import UpdateUserProfile from "./updateuserprofile";
 import './user.css'
 
 const UserProfile=()=>{
+
+
+    const bengali = localStorage.getItem('bengali')
+    const english = localStorage.getItem('english')
+    
     const user = JSON.parse(localStorage.getItem('currentuser'))
 
-    console.log(user)
+   
 
     return(
         <div>
-       
 
-          <div class="card card-1" style={{width:'80%' , marginLeft:'auto' , marginRight:'auto' , borderRadius:'20px' , backgroundColor:'#f3f1f1' }} >
+{
+        bengali ? ( <p>
+
+
+
+
+
+
+
+
+
+
+
+<div class="card card-1" style={{width:'80%' , marginLeft:'auto' , marginRight:'auto' , borderRadius:'20px' , backgroundColor:'#f3f1f1' }} >
+
+         
+<h2 className='user-name' >{user.name} {user.lname} </h2>
+<p className='user-exp'style={{color:'black'}} > {user.email}  </p>
+<p className='user-id' >ব্যবহারকারী আইডি: {user._id} </p>
+
+<hr/>
+<div id='containerbox' >
+
+<Link to='/updateuser/update' style={{textDecoration:'none' , color:'black' }} > 
+<div className='boxb' style={{width:'80%' , color:'white' , backgroundColor:'black' }}  >
+
+আমার অ্যাকাউন্ট
+    
+</div>
+</Link>
+
+<Link to='/updateuser/bookinglist' >
+<div className='boxb' style={{width:'80%' , color:'white' , backgroundColor:'black' }}  >
+
+আমার বুকিং
+    
+</div>
+</Link>
+
+</div>
+
+
+
+<br/><br/>
+
+          </div>
+
+          <Switch>
+
+<Route path="/updateuser/update" component={UpdateUserProfile}   ></Route>
+
+<Route path="/updateuser/bookinglist"    ><BookingList userid={user._id} /></Route>
+</Switch>
+
+
+
+
+
+
+
+
+        </p> ) : ( <p>
+          {
+            english ? (
+              <p>
+
+
+
+
+
+
+
+
+
+<div class="card card-1" style={{width:'80%' , marginLeft:'auto' , marginRight:'auto' , borderRadius:'20px' , backgroundColor:'#f3f1f1' }} >
 
          
 <h2 className='user-name' >{user.name} {user.lname} </h2>
@@ -53,6 +131,83 @@ const UserProfile=()=>{
 
 <Route path="/updateuser/bookinglist"    ><BookingList userid={user._id} /></Route>
 </Switch>
+
+
+
+
+
+
+
+              </p>
+            ) : (
+              <p>
+
+
+
+
+
+
+
+
+
+
+
+<div class="card card-1" style={{width:'80%' , marginLeft:'auto' , marginRight:'auto' , borderRadius:'20px' , backgroundColor:'#f3f1f1' }} >
+
+         
+<h2 className='user-name' >{user.name} {user.lname} </h2>
+<p className='user-exp'style={{color:'black'}} > {user.email}  </p>
+<p className='user-id' >User Id: {user._id} </p>
+
+<hr/>
+<div id='containerbox' >
+
+<Link to='/updateuser/update' style={{textDecoration:'none' , color:'black' }} > 
+<div className='boxb' style={{width:'80%' , color:'white' , backgroundColor:'black' }}  >
+
+    Update My Account
+    
+</div>
+</Link>
+
+<Link to='/updateuser/bookinglist' >
+<div className='boxb' style={{width:'80%' , color:'white' , backgroundColor:'black' }}  >
+
+    My Bookings
+    
+</div>
+</Link>
+
+</div>
+
+
+
+<br/><br/>
+
+          </div>
+
+          <Switch>
+
+<Route path="/updateuser/update" component={UpdateUserProfile}   ></Route>
+
+<Route path="/updateuser/bookinglist"    ><BookingList userid={user._id} /></Route>
+</Switch>
+
+
+
+
+
+
+
+                </p>
+            )
+          }
+        </p> )
+      }
+
+
+       
+
           
 
         </div>
