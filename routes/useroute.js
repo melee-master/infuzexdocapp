d4ee0d0c39e180f242ae6d154bcf23b3c4b86eaf
Awanish Mishra <55555736+hsinawa@users.noreply.github.com>
2022-06-26 05:24:57 +0530

    Add files via upload

--- routes/useroute.js
+++ routes/useroute.js
@@ -8,10 +8,10 @@ const User = require('../models/usermodel')
 router.post('/register', (req,res)=>{
 
 
-    User.find({email:req.body.email} , (err,docs)=>{
+    User.find({contactnumber:req.body.contactnumber} , (err,docs)=>{
         if(docs.length>0)
         {
-            return res.status(400).send({message:'Email Already Registered'})
+            return res.status(400).send({message:'Phone Number Already Registered'})
         }
         else{
             const user = new User(
@@ -62,7 +62,7 @@ router.post('/register', (req,res)=>{
 
 router.post('/login' , (req,res)=>{
 
-    User.find({email:req.body.email , password:req.body.password} , (err,docs)=>{
+    User.find({contactnumber:req.body.email , password:req.body.password} , (err,docs)=>{
 
         if(docs.length>0)
         {
@@ -90,8 +90,9 @@ router.post('/update' , (req,res)=>{
     const {userid , userupdate} = req.body
     User.findByIdAndUpdate({_id : userid} , {
         name : userupdate.name ,
+        lname:userupdate.lname ,
         email: userupdate.email ,
-        password :userupdate.password
+        contactnumber:userupdate.contactnumber
     } , (err)=>{
 
         if(err)
@@ -137,65 +138,61 @@ router.post('/deleteuser' , (req,res)=>{
 
 
 
-var nodemailer = require('nodemailer');
+// var nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
-//   service: 'gmail',
-host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: 'awanishsampleprojects@gmail.com',
-    pass: 'grjmaxulvkdyfdvr'
-  }
-});
+// var transporter = nodemailer.createTransport({
+// //   service: 'gmail',
+// host: 'smtp.gmail.com',
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: 'awanishsampleprojects@gmail.com',
+//     pass: 'grjmaxulvkdyfdvr'
+//   }
+// });
 
 
 
 
 router.post('/forgetpassword' , (req,res)=>{
 
-    const {email} = req.body
+    const {contactnumber} = req.body
 
     // getidbymail(req.body.email)
-
+console.log('The Phone Number rec is' ,req.body.contactnumber  )
     var idvariable 
     var strires
 
-    User.find({email:req.body.email} , (err,res)=>{
+    User.find({contactnumber:req.body.contactnumber} , (err,docs)=>{
         if(err)
         {
-            console.log('Email Not Registered')
-            console.log(`Something Went Wrong with the email id : ${err} `)
+            console.log('Number Not Registered')
+            console.log(`Something Went Wrong with the Phone Number : ${err} `)
             return  res.status(400).json({message:`Something Went Wrong  `})
 
         }
         else
         {
-            strires = JSON.stringify(res)
             
-           
-
-
-            var mailOptions = {
-                            from: 'awanishsampleprojects@gmail.com',
-                            to: `${req.body.email}`,
-                            subject: 'Password Reset',
-                            text: `These are your details ${res} .
-                           
-                            Please note your User ID . i.e the ObjectID , as it is required. 
-
-                            `
-                          };
-                          
-                          transporter.sendMail(mailOptions, function(error, info){
-                            if (error) {
-                              console.log(error);
-                            } else {
-                              console.log('Email sent: ' + info.response);
-                            }
-                          });
             
+            // console.log('The Data is' , docs )
+
+            
+
+            const localsave = {
+                name : docs[0].name ,
+                _id: docs[0]._id ,
+                
+                contactnumber:docs[0].contactnumber ,
+                lname:docs[0].lname
+            }
+
+// console.log('The Local Save is' , localsave )
+
+          
+
+            res.send(localsave)
+
            
             
         }
@@ -229,6 +226,24 @@ router.post('/resetpassword' , (req,res)=>{
 } )
 
 
+
+router.post('/getuserbyid' , (req,res)=>{
+
+
+    
+    User.find( {_id : req.body.userid} , (err, docs)=>{
+        if(!err)
+        {
+           
+             res.send(docs[0])
+        }
+        else
+        {
+            return res.status(400).json({message:`There's an error ${err} `})
+        }
+
+    } )
+} )
 
 
 
